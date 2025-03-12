3ce39e4728cae0c13fc305ef7bb1603d84abd74d
Darshan M R <156114799+darshu-web@users.noreply.github.com>
2025-03-12 15:39:09 +0530

    Update f1.java

--- f1.java
+++ f1.java
@@ -1,5 +1,6 @@
-public class helloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class helloWorld {  // Declares the class
+    public static void main(String[] args) {  // Main method to start the program
+        System.out.println("Hello, World!");  // Prints "Hello, World!" to the console
     }
 }
+
